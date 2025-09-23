5d4072eeaa217677e2d47f9eaebd4b07af4ac3a1
Yuuki Motai <yuus.saddharma.training@gmail.com>
2025-09-23 14:32:25 +0900

    altの修正2

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -30,8 +30,9 @@ export default async function InvoicesTable({
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
-                        height={28} />
-                        alt="image"
+                        height={28}
+                        alt={"image"} />
+                        
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
@@ -89,7 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt="image" />
+                        alt={"image"} />
                       <p>{invoice.name}</p>
                     </div>
                   </td>
